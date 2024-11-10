2d4c18510b60a87129c000a23a36b7a264813e9a
skiran <skiran@akamai.com>
2024-11-10 17:12:12 +0530

    maded changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome To Ashok IT...!!
+          Welcome To Demo By Surya...!!
         </p>
         <a
           className="App-link"
-          href="https://ashokitech.com/"
+          href="https://www.linkedin.com/in/surya-kiran-jammula-008859160/"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Visit Our Website
+         Visit My Linkedin Profile
         </a>
       </header>
     </div>
